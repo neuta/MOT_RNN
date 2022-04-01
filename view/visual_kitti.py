c7665e7a85e37e5a89f516a36ec273035945a132
neuta <nicolas.llanos@uao.edu.co>
2022-04-01 12:53:37 -0500

    View Data

---
+++ view/visual_kitti.py
@@ -0,0 +1,37 @@
+import mayavi.mlab as mlab
+import numpy as np
+import torch
+import pandas as pd
+
+import common_utils as utl
+
+from visual_utils import visualize_utils_3D as V
+
+
+def parse_config():
+
+	parser = argparse.ArgumentParser(description='arg parser')
+	parser.add_argument('--data_path', type=str, default='demo_data', help='specify the point cloud data file or directory')
+
+	args = parser.parse_args()
+
+	return args
+
+
+def main():
+
+	#args = parse_config()
+	
+
+    #print("Start")
+    
+
+
+
+    #print('Visualization done.')
+
+
+
+
+if __name__ == '__main__':
+    main()
